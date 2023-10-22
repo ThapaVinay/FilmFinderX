82665ce3d77ead623c4a0552a161d6242dbbe457
lonewolf <thechampthapa@gmail.com>
2023-10-22 20:23:39 +0530

    tailwind css added with some fixes

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "../screens/HomeScreen"
 
 const Stack = createNativeStackNavigator();
-
-import React from 'react'
 
 const AppNavigation = () => {
     return (
