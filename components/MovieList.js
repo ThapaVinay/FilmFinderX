98dcba06ed6cd0f91e532aa84ee4171412a79bf6
lonewolf <thechampthapa@gmail.com>
2023-10-27 20:33:08 +0530

    cast section added

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,24 +1,27 @@
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, ScrollView, TouchableWithoutFeedback, Image, Dimensions, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { View, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image, Dimensions } from "react-native";
+import { moviesData } from '../constants'
+import { useNavigation } from '@react-navigation/native';
+import { fallbackMoviePoster, image185, image342, poster342 } from '../api/moviedb';
 import { styles } from '../theme';
+const { width, height } = Dimensions.get('window');
 
-var { width, height } = Dimensions.get('window');
-
-const MovieList = ({ title, data }) => {
-
-    const movieName = "OMG 2";
+export default function MovieList({ title, hideSeeAll, data }) {
     const navigation = useNavigation();
-
     return (
         <View className="mb-8 space-y-4">
+
             <View className="mx-4 flex-row justify-between items-center">
-                <Text className="text-white text-xl">
-                    {title}
-                </Text>
-                <TouchableOpacity>
-                    <Text style={styles.text} className="text-lg"> See All</Text>
-                </TouchableOpacity>
+                <Text className="text-white text-lg">{title}</Text>
+                {
+                    !hideSeeAll && (
+                        <TouchableOpacity>
+                            <Text style={styles.text} className="text-lg">See All</Text>
+                        </TouchableOpacity>
+                    )
+                }
+
+
             </View>
 
             <ScrollView
@@ -31,21 +34,17 @@ const MovieList = ({ title, data }) => {
                         return (
                             <TouchableWithoutFeedback
                                 key={index}
-                                onPress={() => navigation.navigate("Movie", item)}
+                                onPress={() => navigation.push('Movie', item)}
                             >
                                 <View className="space-y-1 mr-4">
                                     <Image
-                                        source={require('../assets/images/moviePoster1.png')}
-                                        className="rounded-xl"
-                                        style={{
-                                            width: width * 0.33,
-                                            height: height * 0.22
-                                        }}
+                                        source={{ uri: image185(item.poster_path) || fallbackMoviePoster }}
+                                        className="rounded-3xl"
+                                        style={{ width: width * 0.33, height: height * 0.22 }}
                                     />
-
                                     <Text className="text-neutral-300 ml-1">
                                         {
-                                            movieName.length > 14 ? movieName.slice(0, 14) + '...' : movieName
+                                            item.title.length > 14 ? item.title.slice(0, 14) + '...' : item.title
                                         }
                                     </Text>
                                 </View>
@@ -54,9 +53,6 @@ const MovieList = ({ title, data }) => {
                     })
                 }
             </ScrollView>
-
         </View>
     )
 }
-
-export default MovieList
